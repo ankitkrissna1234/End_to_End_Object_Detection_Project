1fbf6132525daec315dfffb2eb9e4a68f9f4b66c
ankitkrissna1234 <ankitkusahu7846@gmail.com>
2024-05-10 12:48:30 +0530

    requrements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="signLanguages",
+    version="0.0.0",
+    author="Ankit",
+    author_email="sahuankitkumar205@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
