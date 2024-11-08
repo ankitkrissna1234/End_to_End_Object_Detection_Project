3532ad9881e629dd66b582087616ea6c7c8d1588
ankitkrissna1234 <ankitkusahu7846@gmail.com>
2024-11-08 22:43:32 +0530

    data validation and ultratics file added

--- signLanguage/constant/training_pipeline/__init__.py
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -10,4 +10,26 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
-DATA_DOWNLOAD_URL: str = "https://github.com/ankitkrissna1234/Datasets_For_Projects/raw/refs/heads/main/Sign%20Language%20Detection.zip"
+DATA_DOWNLOAD_URL: str = "https://github.com/ankitkrissna1234/Datasets_For_Projects/raw/refs/heads/main/Sign_Language_Dataset.zip"
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["test", "train", "data.yaml"]
+
+
+"""
+MODEL TRAINER related constant start with MODEL_TRAINER var name
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolo11n.pt"
+
+MODEL_TRAINER_NO_EPOCHS: int = 50
+
+MODEL_TRAINER_IMAGE_SIZE: int = 640
